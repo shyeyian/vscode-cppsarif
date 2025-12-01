686fc881a61b4381e28ccef0c8ed48dd25b69301
anonymouspc <shyeyian@petalmail.com>
2025-12-01 14:05:45 +0800

    update

--- upload.js
+++ upload.js
@@ -1,3 +1,6 @@
 let child_process = require("child_process")
-child_process.exec("git commit -m update")
-child_process.exec("vsce publish patch")
+
+child_process.execSync("git add .")
+child_process.execSync("git commit -m 'update'")
+child_process.execSync("git push")
+child_process.execSync("vsce publish patch")
