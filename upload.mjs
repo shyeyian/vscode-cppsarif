34835dfb37621ac0b3679b20edd6450f613f9f4d
shyeyian <shyeyian@icloud.com>
2026-01-18 13:58:44 +0800

    update

--- upload.mjs
+++ upload.mjs
@@ -1,29 +1,21 @@
-import fs from "fs";
-import child_process from "child_process"
-import util from "util"
+import child_process from 'child_process'
+import fs            from 'fs'
+import util          from 'util'
 child_process.promises = new Object()
-child_process.promises.exec = util.promisify(child_process.exec)
+child_process.promises.execFile = util.promisify(child_process.execFile)
 
 // Git pull
-await child_process.promises.exec("git pull")
+child_process.execFile
+await child_process.promises.execFile('git', ['pull'])
 
 // Git commit
 try {
-    await child_process.promises.exec("git add .")
-    await child_process.promises.exec("git commit -m update")
+    await child_process.promises.execFile('git', ['add', '.'])
+    await child_process.promises.execFile('git', ['commit', '-m', 'update'])
 } catch (_) { }
 
 // Vsce upload
-try {
-    await fs.promises.access("vsce-token.txt")
-} catch (_) {
-    throw new Error("failed to upload vscode extension because vsce-token.txt is not found")
-}
-try {
-    await child_process.promises.exec(`vsce publish patch --pat ${await fs.promises.readFile("vsce-token.txt")}`)
-} catch (_) {
-    throw new Error("failed to upload vsce extension") // Avoid vsce-token to be printed in stderr.
-}
+await child_process.promises.execFile('vsce', ['publish', 'patch', '--pat', await fs.promises.readFile('vsce-token.txt')])
 
 // Git push
-await child_process.promises.exec("git push")
+await child_process.promises.execFile('git', ['push'])
